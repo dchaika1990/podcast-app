08eb884dce01f4451d50b3deab16ca53d5f66d41
dchaika1990 <dchaika@inbox.ru>
2020-10-11 00:06:10 +0300

    form validation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+import {isValid} from "./utils";
 import './style.css'
 
-console.log('App working')
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(e) {
+    e.preventDefault()
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        submitBtn.disabled = true
+        //Async request to server to save question
+        console.log('Question', question)
+        input.value = ''
+        input.className = ''
+        submitBtn.disabled = false
+    }
+}
